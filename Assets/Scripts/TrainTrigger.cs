113b5e812b043279946c8ee876fd1385958fc36c
gh1928 <oos1928@gmail.com>
2024-02-28 10:26:08 +0900

    모네 에셋들 추가

---
+++ Assets/Scripts/TrainTrigger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainTrigger : MonoBehaviour
+{
+    public Animator animator;
+
+    public void Start()
+    {
+        animator.SetTrigger("Trigger");
+    }
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (!other.CompareTag("Player"))
+    //        return;
+
+    //    animator.SetTrigger("Trigger");
+    //}
+}
